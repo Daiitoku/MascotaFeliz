48c64576e87b354ce225793ae8bffa7cc30c7d49
JMateoC <mateo.mogollon1001@gmail.com>
2022-09-10 18:26:15 -0500

    Adición de IRepositorioVeterinario, RepositorioVeterinario y cambios en el Program.cs

--- MascotaFeliz.App/MascotaFeliz.App.Consola/Program.cs
+++ MascotaFeliz.App/MascotaFeliz.App.Consola/Program.cs
@@ -8,14 +8,15 @@ namespace MascotaFeliz.App.Consola
     class Program
     {
         private static IRepositorioDueno _repoDueno = new RepositorioDueno(new Persistencia.AppContext());
+        private static IRepositorioVeterinario _repoVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //AddDueno();
+            AddVeterinario();
 
-            AddDueno();
         }
-
         private static void AddDueno()
         {
             var dueno = new Dueno
@@ -29,5 +30,20 @@ namespace MascotaFeliz.App.Consola
             };
             _repoDueno.AddDueno(dueno);
         }
+
+        private static void AddVeterinario()
+        {
+            var veterinario = new Veterinario
+            {
+                //Cedula = "1212",
+                Nombres = "Alvaro",
+                Apellidos = "Torres",
+                Direccion = "Encima de un puente",
+                Telefono = "987456321",
+                TarjetaProfesional = "Zootecnia"
+            };
+            _repoVeterinario.AddVeterinario(veterinario);
+        }
+
     }
 }
