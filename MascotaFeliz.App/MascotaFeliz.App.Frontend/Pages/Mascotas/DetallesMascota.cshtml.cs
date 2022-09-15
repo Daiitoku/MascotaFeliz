b27ecf271c82e3704143702fb87cf319c7697585
Daiitoku <ccbm0906@gmail.com>
2022-09-14 21:52:31 -0500

    DetalleMascota

---
+++ MascotaFeliz.App/MascotaFeliz.App.Frontend/Pages/Mascotas/DetallesMascota.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
+
+namespace MascotaFeliz.App.Frontend.Pages
+{
+    public class DetallesMascotaModel : PageModel
+    {
+        private static IRepositorioMascota _repoMascota = new RepositorioMascota(new Persistencia.AppContext());
+        public Mascota mascota {get;set;}
+
+
+        public IActionResult OnGet(int mascotaId)
+        {
+            mascota = _repoMascota.GetMascota(mascotaId);
+            if(mascota == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            else
+            {
+                return Page();
+            }
+         }
+    }
+}
